3b3022f0ec42161a6f24926d7318617f75989341
Pavan Krishna G <150824534+PavanG5@users.noreply.github.com>
2025-04-07 18:51:41 +0530

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+Wireframe Flow:
+1. User inputs story idea
+2. AI presents 3 story paths
+3. User selects path
+4. Story gets generated and narrated
+5. Options to listen or continue
+Future Extensions:
+- Multiplayer story writing
+- Voice cloning (Pro)
+- Public Storyverse of UGC
+Prepared by:
+Gojja Pavan Krishna
+21BCE9614
+Vellore Institute Of Technology, Andhra Pradesh
+pavanpck56@gmail.com
